5f50a73ccfffc569dc4a478d0d6f013162154257
Ridhima Khyalappa <ridhima_khyalappa@heartcore.co.jp>
2023-06-22 15:05:13 +0900

    till structs

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,27 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    input.trim().to_string()
+
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
-}
+    let mut str2 = String::from(input);
+        str2.push_str(" world!");
+        str2
+    }
+
+
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let mut str3 = String::from(input);
+    str3.replace("cars", "balloons")
 }
 
 #[cfg(test)]
